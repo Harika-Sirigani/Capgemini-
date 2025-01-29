b57603496fbaae121d5c9788b7e9dce57d8e1100
Harika-Sirigani <siriganiharika030@gmail.com>
2025-01-29 09:10:06 +0530

    Add files via upload

---
+++ Day1/Hello_foo.py
@@ -0,0 +1,3 @@
+first_name="Ram"
+no=2
+print(f"hello {first_name}, how are you?")
---
+++ Day1/Hello_person.py
@@ -0,0 +1,6 @@
+first_name=input("")
+first_name=input("What is your first name?")
+last_name=input("What is your last name?")
+#first_name="Gopi"
+#last_name="Suresh"
+print(f"Hello {first_name} {last_name}, how are you?")
